8e8c3d8c7624b338a6b5a97b1b40454770d020ad
skcajs <sjackson864@outlook.com>
2023-09-30 16:42:14 +0200

    starting code

--- src/main.js
+++ src/main.js
@@ -1,37 +1,82 @@
 //  Vertex shader program
-const VSHADER_SOURCE = `
+var VSHADER_SOURCE = `
+attribute vec4 a_Position;
 void main() {
-    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_Position = a_Position; 
     gl_PointSize = 10.0;
-}
-`
+}`;
 
 // Fragment Shader program
-const FSHADER_SOURCE = `
+var FSHADER_SOURCE = `
 void main() {
     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }`;
 
 function main() {
-  // Get Canvas Element
-  const canvas = document.getElementById('webgl')
-  if (!canvas) {
-    console.log('Failed to retrieve canvas element')
-    return
-  }
-  // Get the WebGL context
-  const gl = getWebGLContext(canvas)
+    // Retrieve the canvas element
+    var canvas = document.getElementById('webgl');
 
-  // Initialise the shader
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to initialise shaders')
-  }
+    // Get the webGL rendering context
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get rendering context for WebGL.');
+        return;
+    }
 
-  // Set the clear color
-  gl.clearColor(0.0, 0.0, 0.0, 1.0)
-  // Clear the canvas
-  gl.clear(gl.COLOR_BUFFER_BIT)
+    // Initialise shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialise shaders.');
+        return;
+    }
 
-  // Draw Point
-  gl.drawArrays(gl.POINTS, 0, 1)
+    // Set positions of vertices
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices.');
+        return;
+    }
+
+    // Set colour for clearing <canvas>
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Clear <canvas> 
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Draw three points
+    gl.drawArrays(gl.POINTS, 0, n); // n is 3
+}
+
+function initVertexBuffers(gl) {
+    // Define vertices
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ]);
+    var n = 3; // The number of vertices
+
+    // Create a buffer objects
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create buffer object');
+        return -1;
+    }
+
+    // Bind buffer object to a target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // Write data into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    // Get storage locations for shader attributes
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get storage location of a_Position');
+        return;
+    }
+
+    // Assign buffer object to a_Position (attribute) variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // Enable assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
 }
